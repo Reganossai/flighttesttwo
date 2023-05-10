0b01004e6556517c2385d1f2767d8507040b5c7f
Ossai Reagan <ossaireagano@gmail.com>
2023-05-11 00:08:54 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginregisterComponent } from './loginregister/loginregister.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './guard/auth.guard';
+
+
+const routes: Routes = [
+{
+  path:'',
+  redirectTo: 'loginregister',
+  pathMatch: 'full'
+},
+{
+  path:'loginregister',
+  component:LoginregisterComponent
+},
+{
+  path:'dashboard',
+  component:DashboardComponent,
+  canActivate: [AuthGuard]
+}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
